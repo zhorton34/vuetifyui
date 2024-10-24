479ea882dabc9082aa5b9343a753203586e9de51
Zak Horton <zhorton999@gmail.com>
2024-10-23 20:36:54 -0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,151 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+// PWA Config
+const title = "Vuetify 3 + Nuxt 3 Starter";
+const shortTitle = "Vuetify 3 + Nuxt 3 Starter";
+const description = "Template to get you up and running with Nuxt 3 & Vuetify 3";
+const image = "https://vuetify3nuxt3starter.behonbaker.com/starter.png";
+const url = "https://vuetify3nuxt3starter.behonbaker.com/";
+const author = "Behon Baker";
+const themeColor = "#4f46e5";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  // import styles
+  css: ["@/assets/main.scss"],
+
+  devtools: { enabled: true },
+
+  // enable takeover mode
+  typescript: { shim: false },
+
+  build: { transpile: ["vuetify"] },
+
+  // Based on docs found here - https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
+  modules: [
+    "@vite-pwa/nuxt",
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        config.plugins ||= [];
+        config.plugins.push(vuetify());
+      });
+    },
+  ],
+
+  app: {
+    head: {
+      title: "Vuetify 3 + Nuxt 3 Starter",
+      titleTemplate: "%s | Vuetify 3 + Nuxt 3 Starter",
+      link: [
+        { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
+        { rel: "preconnect", href: "https://rsms.me/" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "canonical", href: url },
+      ],
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: description,
+        },
+        { property: "og:site_name", content: title },
+        { hid: "og:type", property: "og:type", content: "website" },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: url,
+        },
+        {
+          hid: "og:image:secure_url",
+          property: "og:image:secure_url",
+          content: image,
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: title,
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: description,
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: image,
+        },
+        //Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: url,
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: title,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: description,
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: image,
+        },
+      ],
+    },
+  },
+
+  pwa: {
+    includeAssets: ["favicon.ico", "robots.txt"],
+    manifest: {
+      name: shortTitle,
+      short_name: shortTitle,
+      description: description,
+      theme_color: themeColor,
+      lang: "en",
+      background_color: "#ffffff",
+      icons: [
+        {
+          src: "/icons/pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "/icons/pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "/icons/pwa-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "/icons/pwa-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+  },
+
+  compatibilityDate: "2024-07-15",
+});
